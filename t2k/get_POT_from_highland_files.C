ea3c0dc5866a0c9696607d2e75a87ae02c5a428a
Adrien BLANCHET <adrien.blanchet@live.fr>
2020-03-11 17:21:38 +0100

    Fast commit.

--- t2k/get_POT_from_highland_files.C
+++ t2k/get_POT_from_highland_files.C
@@ -4,12 +4,17 @@ string __irods_pulled_path__ = "/sps/t2k/ablanche/work/results/irods/run2a";
 
 void get_POT_from_highland_files(){
 
+  DataSample* ds;
+
   auto file_path_list = TToolBox::get_list_of_files_in_subfolders(__irods_pulled_path__, ".root");
   std::sort(file_path_list.begin(), file_path_list.end());
 
   for(int i_file = 0 ; i_file < int(file_path_list.size()); i_file++){
     if(TToolBox::do_string_starts_with_substring(file_path_list[i_file], "NumuCCMultiPiAnalysis")){
-      cerr << file_path_list[i_file] << endl;
+
+      ds = new DataSample((__irods_pulled_path__ + "/" + file_path_list[i_file]).c_str(),kGoodBeamGoodND280)
+      cerr << ds.GetPOT() << endl;
+
     }
   }
 
