9beee8307d3247011fd440619306af06f7a22bd8
Adrien BLANCHET <adrien.blanchet@live.fr>
2020-03-11 17:30:52 +0100

    Fast commit.

--- t2k/get_POT_from_highland_files.C
+++ t2k/get_POT_from_highland_files.C
@@ -1,4 +1,6 @@
 
+
+
 std::vector<std::string> get_list_of_files_in_subfolders(std::string folder_path_, std::string files_extension_ = ""){
   std::string *folder_path = &folder_path_;
   std::string *files_extension = nullptr;
@@ -7,17 +9,24 @@ std::vector<std::string> get_list_of_files_in_subfolders(std::string folder_path
 }
 
 
-string __irods_pulled_path__ = "/sps/t2k/ablanche/work/results/irods/run2a";
+
+
+
+
+
+
+
+std::string __irods_pulled_path__ = "/sps/t2k/ablanche/work/results/irods/run2a";
 
 void get_POT_from_highland_files(){
 
   DataSample* ds;
 
-  std::vector<std::string> file_path_list = TToolBox::get_list_of_files_in_subfolders(__irods_pulled_path__, ".root");
+  std::vector<std::string> file_path_list = get_list_of_files_in_subfolders(__irods_pulled_path__, ".root");
   std::sort(file_path_list.begin(), file_path_list.end());
 
   for(int i_file = 0 ; i_file < int(file_path_list.size()); i_file++){
-    if(TToolBox::do_string_starts_with_substring(file_path_list[i_file], "NumuCCMultiPiAnalysis")){
+    if(do_string_starts_with_substring(file_path_list[i_file], "NumuCCMultiPiAnalysis")){
 
       ds = new DataSample((__irods_pulled_path__ + "/" + file_path_list[i_file]).c_str(),kGoodBeamGoodND280)
       cerr << ds.GetPOT() << endl;
