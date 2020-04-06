4f551dc3099f6f6dc8f69ef11e8bac27cfd7bf91
Adrien BLANCHET <adrien.blanchet@live.fr>
2020-04-06 15:27:24 +0200

    Fast commit.

--- t2k/check_beam_mode.C
+++ t2k/check_beam_mode.C
@@ -1,24 +1,38 @@
 
 
+void lf()
+{
+   TIter next(gROOT->GetListOfGlobals(1));
+   TGlobal *global;
+   while ((global=(TGlobal*)next())) {
+     TString type = global->GetTypeName();
+     if (type=="TFile") {
+       TFile *file = (TFile*)gInterpreter->Calc(global->GetName());
+       if (file && file->IsOpen())
+         printf("%s: %s\n", global->GetName(),file->GetName());
+     }
+   }
+ }
 
+void check_beam_mode(){
 
-void check_beam_mode(int argc, char** argv){
+lf();
 
-  if(argc != 2){
-    return;
-  }
+  // if(argc != 2){
+  //   return;
+  // }
+  //
+  // string flat_file_path(argv[1]);
+  // TFile* flat_file = TFile::Open(flat_file_path.c_str());
+  // TTree* flattree = (TTree*) flat_file->Get("flattree");
+  //
+  // flattree->GetEntry(0);
+  // double run = flattree->GetLeaf("sRun")->GetValue(0);
+  //
+  // if(int(run/1E7) == 9) cout << "1" << endl;
+  // else if(int(run/1E7) == 8) cout << "-1" << endl;
+  // else cout << "0" << endl;
+  //
+  // return;
 
-  string flat_file_path(argv[1]);
-  TFile* flat_file = TFile::Open(flat_file_path.c_str());
-  TTree* flattree = (TTree*) flat_file->Get("flattree");
-
-  flattree->GetEntry(0);
-  double run = flattree->GetLeaf("sRun")->GetValue(0);
-
-  if(int(run/1E7) == 9) cout << "1" << endl;
-  else if(int(run/1E7) == 8) cout << "-1" << endl;
-  else cout << "0" << endl;
-
-  return;
-  
 }
