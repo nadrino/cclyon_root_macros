b05ce3b6100806bf936963901fb99fa186bce12d
Adrien BLANCHET <adrien.blanchet@live.fr>
2020-04-06 15:38:14 +0200

    Fast commit.

--- t2k/check_beam_mode.C
+++ t2k/check_beam_mode.C
@@ -1,38 +1,42 @@
 
 
-void lf()
-{
-   TIter next(gROOT->GetListOfGlobals(1));
-   TGlobal *global;
-   while ((global=(TGlobal*)next())) {
-     TString type = global->GetTypeName();
-     if (type=="TFile") {
-       TFile *file = (TFile*)gInterpreter->Calc(global->GetName());
-       if (file && file->IsOpen())
-         printf("%s: %s\n", global->GetName(),file->GetName());
-     }
-   }
- }
+vector<TFile*> get_list_of_openned_tfiles(){
+
+  vector<TFile*> out_vector;
+
+  TIter next(gROOT->GetListOfGlobals(1));
+  TGlobal *global;
+  while ((global=(TGlobal*)next())) {
+    TString type = global->GetTypeName();
+    if (type=="TFile") {
+      TFile *file = (TFile*)gInterpreter->Calc(global->GetName());
+      if (file && file->IsOpen()){
+        out_vector.emplace_back(file);
+      }
+    }
+  }
+
+}
 
 void check_beam_mode(){
 
-lf();
+  vector<TFile*> openned_tfiles = get_list_of_openned_tfiles();
 
-  // if(argc != 2){
-  //   return;
-  // }
-  //
-  // string flat_file_path(argv[1]);
-  // TFile* flat_file = TFile::Open(flat_file_path.c_str());
-  // TTree* flattree = (TTree*) flat_file->Get("flattree");
-  //
-  // flattree->GetEntry(0);
-  // double run = flattree->GetLeaf("sRun")->GetValue(0);
-  //
-  // if(int(run/1E7) == 9) cout << "1" << endl;
-  // else if(int(run/1E7) == 8) cout << "-1" << endl;
-  // else cout << "0" << endl;
-  //
-  // return;
+  if(openned_tfiles.size() != 1){
+    cerr << "openned_tfiles.size() != 1" << endl;
+    cerr << "Usage : root path/to/root/file.root /path/to/this/script/check_beam_mode.C" << endl;
+  }
+
+  TFile* flat_file = openned_tfiles[0];
+  TTree* flattree = (TTree*) flat_file->Get("flattree");
+
+  flattree->GetEntry(0);
+  double run = flattree->GetLeaf("sRun")->GetValue(0);
+
+  if(int(run/1E7) == 9) cout << "1" << endl;
+  else if(int(run/1E7) == 8) cout << "-1" << endl;
+  else cout << "0" << endl;
+
+  return;
 
 }
