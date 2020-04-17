575deda759551c2ba776129f4c68f422f528f60f
Adrien BLANCHET <adrien.blanchet@live.fr>
2020-04-17 10:17:11 +0200

    Fast commit.

--- t2k_sk/neutrino_tracks_generator.C
+++ t2k_sk/neutrino_tracks_generator.C
@@ -10,12 +10,15 @@ std::map<std::string, double> __event_container__;
 
 TFile* __output_TFile__;
 TRandom3* __root_PRNG__;
+TF1* __earth_density_TF1__;
 
+void build_PREM();
 
 void neutrino_tracks_generator(){
 
-  __root_PRNG__ = new TRandom3(time(NULL));
+  build_PREM();
 
+  __root_PRNG__ = new TRandom3(time(NULL));
 
   __event_container__["X_vertex"] = 0.;
   __event_container__["Y_vertex"] = 0.;
@@ -33,6 +36,7 @@ void neutrino_tracks_generator(){
   __event_container__["SK_solid_angle_vertex"] = 0.;
 
   __output_TFile__ = TFile::Open("output.root", "RECREATE");
+  __earth_density_TF1__->Write("PREM_TF1");
   TTree* events_tree = new TTree("neutrino_tracks", "neutrino_tracks");
   for(auto &observable : __event_container__){
     events_tree->Branch(observable.first.c_str(), &observable.second);
@@ -65,5 +69,88 @@ void neutrino_tracks_generator(){
   events_tree->Write();
   __output_TFile__->Close();
   exit(0);
+
+}
+
+
+void build_PREM(){
+
+  // Preliminary reference Earth model
+  // https://www.sciencedirect.com/science/article/abs/pii/0031920181900467?via%3Dihub
+
+  std::vector<std::string> layer_label;
+  std::vector<double> layer_outer_bound;
+  std::vector<std::vector<double>> layer_polynomial_coefficients;
+
+  std::vector< std::map< std::string, std::vector<double> > > layers_data;
+
+  layer_label.emplace_back("InnerCore");
+  layer_outer_bound.emplace_back(1221.5);
+  layer_polynomial_coefficients.emplace_back({13.0885, 0, -8.8381});
+
+  layer_label.emplace_back("OutterCore");
+  layer_outer_bound.emplace_back(3480.0);
+  layer_polynomial_coefficients.emplace_back({12.5815, -1.2638, -3.6426, —5.5281});
+
+  layer_label.emplace_back("LowerMantle-1");
+  layer_outer_bound.emplace_back(3630.0);
+  layer_polynomial_coefficients.emplace_back({7.9565, -6.4761, 5.5283, -3.0807});
+
+  layer_label.emplace_back("LowerMantle-2");
+  layer_outer_bound.emplace_back(5600.0);
+  layer_polynomial_coefficients.emplace_back({7.9565, -6.4761, 5.5283, —3.0807});
+
+  layer_label.emplace_back("LowerMantle-3");
+  layer_outer_bound.emplace_back(5701.0);
+  layer_polynomial_coefficients.emplace_back({7.9565, —6.4761, 5.5283, —3.0807});
+
+  layer_label.emplace_back("TransitionZone-1");
+  layer_outer_bound.emplace_back(5771.0);
+  layer_polynomial_coefficients.emplace_back({5.3197, —1.4836});
+
+  layer_label.emplace_back("TransitionZone-2");
+  layer_outer_bound.emplace_back(5971.0);
+  layer_polynomial_coefficients.emplace_back({11.2494, —8.0298});
+
+  layer_label.emplace_back("TransitionZone-3");
+  layer_outer_bound.emplace_back(6151.0);
+  layer_polynomial_coefficients.emplace_back({7.1089, —3.8045});
+
+  layer_label.emplace_back("LVZ");
+  layer_outer_bound.emplace_back(6291.0);
+  layer_polynomial_coefficients.emplace_back({2.6910, 0.6924});
+
+  layer_label.emplace_back("LID");
+  layer_outer_bound.emplace_back(6346.6);
+  layer_polynomial_coefficients.emplace_back({2.6910, O.6924});
+
+  layer_label.emplace_back("Crust-1");
+  layer_outer_bound.emplace_back(6356.0);
+  layer_polynomial_coefficients.emplace_back({2.900});
+
+  layer_label.emplace_back("Crust-2");
+  layer_outer_bound.emplace_back(6368.0);
+  layer_polynomial_coefficients.emplace_back({2.600});
+
+  std::vector<TF1*> functions_list;
+  double last_bound = 0.;
+  for(int i_fct = 0 ; i_fct < layer_label.size() ; i_fct++){
+
+    std::stringstream formulae;
+    formulae << "("
+    for(int i_exposant = 0 ; i_exposant < layer_polynomial_coefficients[i_fct].size() ; i_exposant++){
+      if(i_exposant != 0) formulae << " + ";
+      formulae << layer_polynomial_coefficients[i_fct] << "*TMath::Power(x," << i_exposant << ")";
+    }
+    formulae << ")"
+    formulae << "*" << "if(x>=" << last_bound << ")*if(x<" << layer_outer_bound[i_exposant] << ")";
+
+    functions_list.emplace_back(
+      new TF1(layer_label[i_fct].c_str(), formulae.str().c_str(), 0., layer_outer_bound.back()+50.);
+    );
+
+  }
+
+  __earth_density_TF1__ = new TF1("PREM", TToolBox::join_vector_string(layer_label, " + ").c_str(), 0., layer_outer_bound.back()+50.);
 
 }
