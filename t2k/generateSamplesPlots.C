4dc01700ac06d96b97a44edf870abb939c5770d7
Adrien BLANCHET <adrien.blanchet@live.fr>
2020-09-02 19:56:25 +0200

    Fast commit.

--- t2k/generateSamplesPlots.C
+++ t2k/generateSamplesPlots.C
@@ -86,7 +86,7 @@ void generateSamplesPlots(){
       for(int iHist = histManualStack.size()-1 ; iHist >= 0 ; iHist--){
         string opt = "SAME";
         if(iHist == histManualStack.size()-1) opt= "";
-        histManualStack->Draw(opt.c_str());
+        histManualStack[iHist]->Draw(opt.c_str());
       }
 
       // cout << " > Drawing: " << stackName << " -> " << 1 + i_fgd*sampleNames.size() + sample.first << endl;
