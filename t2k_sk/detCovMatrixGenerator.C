ea0c31fa8c19d49aaceecc2cc129b0ee41ba2c70
Adrien BLANCHET <adrien.blanchet@live.fr>
2020-10-01 17:59:14 +0200

    Fast commit.

---
+++ t2k_sk/detCovMatrixGenerator.C
@@ -0,0 +1,84 @@
+
+
+TF1* gausFunction = nullptr;
+TFile* outFile = nullptr;
+TTree* outTree = nullptr;
+bool varMapIsHooked = false;
+std::map<double> varMap;
+
+double pickToyParameter(double oneSigma_);
+void init();
+void hookToTree();
+
+void detCovMatrixGenerator()
+{
+
+  init();
+
+  TFile* atmFile = TFile::Open("/sps/t2k/ablanche/resources/T2KSKjoint/sk4_fcmc_18a_fQv6r0_minituple_100yr_05.root");
+  TTree* atm_minituple = (TTree*) atmFile->Get("atm_minituple")
+
+  double nominalCounts = atm_minituple->Draw("", "MReIncLVal>0", "goff");
+
+  std::map<std::string, std::pair<double>> throwingRanges;
+  throwingRanges["b"].first = -10;
+  throwingRanges["b"].second = 10;
+  throwingRanges["log_a"].first = -2;
+  throwingRanges["log_a"].second = 2;
+
+  // L -> a*L + b
+  stringstream rcFormulaeSS;
+  double gausNorm = gausFunction->Integral(-5,5);
+  double sigma = 0.06;
+  int nbThrows = 1E5;
+  for(int iThrow = 0 ; iThrow < nbThrows ; iThrow++){
+    varMap["b"] = (throwingRanges["b"].second - throwingRanges["b"].first)*gRandom->Rndm() + throwingRanges["b"].first;
+    varMap["log_a"] = (throwingRanges["log_a"].second - throwingRanges["log_a"].first)*gRandom->Rndm() + throwingRanges["log_a"].first;
+    varMap["a"] = TMath::Power(10, varMap["log_a"]);
+
+    rcFormulaeSS("");
+    rcFormulaeSS << varMap["a"] << "*MReIncLVal+" << varMap["b"] << ">0";
+    varMap["counts"] = atm_minituple->Draw("", rcFormulaeSS.str().c_str(), "goff");
+    varMap["delta_counts"] = varMap["counts"] - nominalCounts;
+    varMap["delta_counts_over_counts"] = varMap["delta_counts"]/nominalCounts;
+
+    varMap["weight"] = gausFunction->Eval(varMap["delta_counts_over_counts"]/sigma)/gausNorm;
+
+    if(not varMapIsHooked){
+      hookToTree();
+    }
+    outTree->Fill();
+  }
+  outFile->WriteTObject(outTree, "outTree");
+  outFile->Close();
+
+}
+
+void hookToTree()
+{
+    outFile = TFile::Open("outFile.root", "RECREATE");
+    outTree = new TTree("outTree", "outTree");
+    for(const auto& varMapPair : varMap){
+      outTree->Branch(varMapPair.first.c_str(), &varMapPair.second);
+    }
+    varMapIsHooked = true;
+}
+
+void init()
+{
+  if(gausFunction == nullptr){
+    gausFunction = new TF1("gausFunction", "gaus", -5, 5);
+    gausFunction->SetParameter("Constant", 1);
+    gausFunction->SetParameter("Mean", 0);
+    gausFunction->SetParameter("Sigma", 1);
+  }
+}
+
+double pickToyParameter(double oneSigma_)
+{
+
+
+
+  return gausFunction->GetRandom()*oneSigma_;
+
+}
--- test.C
+++ test.C
@@ -3,9 +3,8 @@
 
 void test(){
 
-  vector<double> FHCNumuCCOth_Mom_Bin = {0. , 500. , 700. , 1000., 1250., 1500., 2000., 3000., 10000.};
-  vector<double> FHCNumuCCOth_Cos_Bin = {-1.0 , 0.7 , 0.8 , 0.85, 0.90, 0.93, 0.95, 0.96,
-                                         0.97, 0.98, 0.99, 1.0};
+  vector<double> FHCNumuCCOth_Mom_Bin = {0. , 600. , 1000., 1250., 2000., 4000., 30000.};
+  vector<double> FHCNumuCCOth_Cos_Bin = {-1.0, 0.7, 0.8, 0.86, 0.9 , 0.93, 0.95, 0.97, 0.99, 1.0};
 
   for(int iCosBin = 0 ; iCosBin < FHCNumuCCOth_Cos_Bin.size()-1 ; iCosBin++){
     for( int iMomBin = 0 ; iMomBin < FHCNumuCCOth_Mom_Bin.size()-1 ; iMomBin++){
