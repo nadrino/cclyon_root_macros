c88f7b028960916f2b25b1355ea5c4bcdd62bf4d
Adrien BLANCHET <adrien.blanchet@live.fr>
2020-10-01 18:01:36 +0200

    Fast commit.

--- t2k_sk/detCovMatrixGenerator.C
+++ t2k_sk/detCovMatrixGenerator.C
@@ -4,7 +4,7 @@ TF1* gausFunction = nullptr;
 TFile* outFile = nullptr;
 TTree* outTree = nullptr;
 bool varMapIsHooked = false;
-std::map<double> varMap;
+std::map<std::string, double> varMap;
 
 double pickToyParameter(double oneSigma_);
 void init();
@@ -16,11 +16,11 @@ void detCovMatrixGenerator()
   init();
 
   TFile* atmFile = TFile::Open("/sps/t2k/ablanche/resources/T2KSKjoint/sk4_fcmc_18a_fQv6r0_minituple_100yr_05.root");
-  TTree* atm_minituple = (TTree*) atmFile->Get("atm_minituple")
+  TTree* atm_minituple = (TTree*) atmFile->Get("atm_minituple");
 
   double nominalCounts = atm_minituple->Draw("", "MReIncLVal>0", "goff");
 
-  std::map<std::string, std::pair<double>> throwingRanges;
+  std::map<std::string, std::pair<double, double>> throwingRanges;
   throwingRanges["b"].first = -10;
   throwingRanges["b"].second = 10;
   throwingRanges["log_a"].first = -2;
@@ -36,7 +36,8 @@ void detCovMatrixGenerator()
     varMap["log_a"] = (throwingRanges["log_a"].second - throwingRanges["log_a"].first)*gRandom->Rndm() + throwingRanges["log_a"].first;
     varMap["a"] = TMath::Power(10, varMap["log_a"]);
 
-    rcFormulaeSS("");
+    rcFormulaeSS.str("");
+    rcFormulaeSS.clear();
     rcFormulaeSS << varMap["a"] << "*MReIncLVal+" << varMap["b"] << ">0";
     varMap["counts"] = atm_minituple->Draw("", rcFormulaeSS.str().c_str(), "goff");
     varMap["delta_counts"] = varMap["counts"] - nominalCounts;
