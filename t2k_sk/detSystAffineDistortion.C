55d56fa1c3c4095ba7e9a72d367aafe3389c9716
Adrien BLANCHET <adrien.blanchet@live.fr>
2020-10-14 15:59:34 +0200

    Fast commit.

--- t2k_sk/detSystAffineDistortion.C
+++ t2k_sk/detSystAffineDistortion.C
@@ -19,18 +19,18 @@ void detSystAffineDistortion()
 
   TFile* atmFile = TFile::Open("/sps/t2k/ablanche/resources/SK-T2K-Joint/sk4_fcmc_18a_fQv6r0_minituple_100yr_05.root");
   TTree* atm_minituple = (TTree*) atmFile->Get("atm_minituple");
-  atm_minituple->SetBranchStatus("*", false);
-  atm_minituple->SetBranchStatus("MReIncLVal", true);
+  // atm_minituple->SetBranchStatus("*", false);
+  // atm_minituple->SetBranchStatus("MReIncLVal", true);
+  //
+  // Float_t MReIncLVal;
+  // atm_minituple->SetBranchAddress("MReIncLVal", &MReIncLVal);
+  // for(int iEntry = 0 ; iEntry < atm_minituple->GetEntries() ; iEntry++){
+  //     GenericToolbox::displayProgressBar(iEntry, atm_minituple->GetEntries(), "Reading...");
+  //   atm_minituple->GetEntry(iEntry);
+  //   observableValueList.emplace_back(MReIncLVal);
+  // }
 
-  Float_t MReIncLVal;
-  atm_minituple->SetBranchAddress("MReIncLVal", &MReIncLVal);
-  for(int iEntry = 0 ; iEntry < atm_minituple->GetEntries() ; iEntry++){
-      GenericToolbox::displayProgressBar(iEntry, atm_minituple->GetEntries(), "Reading...");
-    atm_minituple->GetEntry(iEntry);
-    observableValueList.emplace_back(MReIncLVal);
-  }
-
-  double nominalCounts = atm_minituple->Draw("", "MReIncLVal>0", "goff");
+  double nominalCounts = atm_minituple->Draw("", "MReIncLVal>-0.25", "goff");
 
   std::map<std::string, std::pair<double, double>> throwingRanges;
   throwingRanges["b"].first = -15;
