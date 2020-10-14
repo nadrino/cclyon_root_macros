80488c248fa093b8dca241d09e62bc7733a9121d
Adrien BLANCHET <adrien.blanchet@live.fr>
2020-10-14 16:35:42 +0200

    Fast commit.

--- t2k_sk/detSystAffineDistortion.C
+++ t2k_sk/detSystAffineDistortion.C
@@ -33,10 +33,12 @@ void detSystAffineDistortion()
   double nominalCounts = atm_minituple->Draw("", "MReIncLVal>-0.25", "goff");
 
   std::map<std::string, std::pair<double, double>> throwingRanges;
-  throwingRanges["b"].first = -2;
-  throwingRanges["b"].second = 2;
-  // throwingRanges["log_a"].first = -1;
-  // throwingRanges["log_a"].second = 4;
+
+  throwingRanges["b"].first = -15;
+  throwingRanges["b"].second = 15;
+
+  throwingRanges["a"].first = 0.1;
+  throwingRanges["a"].second = 10;
 
   TH1D* deviation_hist = new TH1D("deviation_hist", "deviation_hist", 100, -0.2, 0.2);
 
@@ -54,7 +56,7 @@ void detSystAffineDistortion()
       varMap[throwingRange.first] = (throwingRange.second.second - throwingRange.second.first)*gRandom->Rndm() + throwingRange.second.first;
     }
     // varMap["a"] = TMath::Power(10, varMap["log_a"]);
-    varMap["a"] = 1;
+    // varMap["a"] = 1;
     // varMap["b"] = TMath::Power(10, varMap["log_b"]);
     // if(!bSignIsPositive) varMap["b"] *= -1;
 
