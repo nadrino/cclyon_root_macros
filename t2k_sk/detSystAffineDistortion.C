1d6937e3bba91000f30a9836e3adf7afb2c5b38f
Adrien BLANCHET <adrien.blanchet@live.fr>
2020-10-14 16:50:47 +0200

    Fast commit.

--- t2k_sk/detSystAffineDistortion.C
+++ t2k_sk/detSystAffineDistortion.C
@@ -37,8 +37,8 @@ void detSystAffineDistortion()
   throwingRanges["b"].first = -15;
   throwingRanges["b"].second = 15;
 
-  throwingRanges["a"].first = 0.1;
-  throwingRanges["a"].second = 10;
+  throwingRanges["log_a"].first = -1;
+  throwingRanges["log_a"].second = 2;
 
   TH1D* deviation_hist = new TH1D("deviation_hist", "deviation_hist", 100, -0.2, 0.2);
 
@@ -55,7 +55,7 @@ void detSystAffineDistortion()
     for(auto &throwingRange : throwingRanges){
       varMap[throwingRange.first] = (throwingRange.second.second - throwingRange.second.first)*gRandom->Rndm() + throwingRange.second.first;
     }
-    // varMap["a"] = TMath::Power(10, varMap["log_a"]);
+    varMap["a"] = TMath::Power(10, varMap["log_a"]);
     // varMap["a"] = 1;
     // varMap["b"] = TMath::Power(10, varMap["log_b"]);
     // if(!bSignIsPositive) varMap["b"] *= -1;
