18124c06f881490ba51adbe886cc0d5d7ad5c9c0
Adrien BLANCHET <adrien.blanchet@live.fr>
2020-11-03 09:44:00 +0100

    Fast commit.

---
+++ t2k_sk/detSystParTest.C
@@ -0,0 +1,128 @@
+
+std::string mcFilePath = "$RESOURCES_DIR/SK-T2K-Joint/sk4_fcmc_18a_fQv6r0_minituple_100yr_05.root";
+
+enum ATMPDEventType{
+  SubGeV_elike_0dcy = 1,
+  SubGeV_elike_1dcy,
+  SubGeV_SingleRing_pi0like,
+  SubGeV_mulike_0dcy,
+  SubGeV_mulike_1dcy,
+  SubGeV_mulike_2dcy,
+  SubGeV_pi0like,
+  MultiGeV_elike_nue,
+  MultiGeV_elike_nuebar,
+  MultiGeV_mulike,
+  MultiRing_elike_nue,
+  MultiRing_elike_nuebar,
+  MultiRing_mulike,
+  MultiRingOther_1,
+  PCStop, // not yet in data
+  PCThru,
+  UpStop_mu,
+  UpThruNonShower_mu,
+  UpThruShower_mu
+};
+
+struct fqEvent{
+  float*** fqmrdir;
+
+  float** fq1rnll;
+  float** fqmrmom;
+
+  float* fqmrnll;
+  float* fqmrifit;
+}
+
+int getBest2RFitID(fqEvent* fqevent);
+float getRCParameter(fqEvent* fqevent);
+
+void detSystParTest(){
+
+  TFile* mcFile = TFile::Open(mcFilePath.c_str());
+  TTree* mcTree = (TTree*) mcFile->Get("atm_minituple");
+
+  fqEvent fqevent;
+  mcTree->SetBranchAddress("fqmrdir", fqmrdir);
+
+  mcTree->SetBranchAddress("fq1rnll", fq1rnll);
+  mcTree->SetBranchAddress("fqmrmom", fqmrmom);
+
+  mcTree->SetBranchAddress("fqmrnll", fqmrnll);
+  mcTree->SetBranchAddress("fqmrifit", fqmrifit);
+
+
+}
+
+
+///////////////////////////////////////////
+float getRCParameter(fqEvent* fqevent){
+
+  // get best 2R ID
+  int ibest = getBest2RFitID(fqevent);
+
+  // get best 1R Likelihood
+  float best1Rnglnl = TMath::Min(fqevent->fq1rnll[0][1],fqevent->fq1rnll[0][2] );
+
+  // get mom of 2nd ring
+  float ringmom = (float)TMath::Min(fqevent->fqmrmom[best2RID][0],fqevent->fqmrmom[best2RID][1]);
+
+  // likelihood difference between 1R and 2R
+  float deltaLnL = best1Rnglnl - fqevent->fqmrnll[best2RID];
+
+  // cut values from fiTQun.cc v4r0
+  float a0 = 150.;
+  float a1 = -0.6;
+
+  // these values determine the cut line
+  float cthresh = a0 + a1*(ringmom);
+  if (!(cthresh>0.)) cthresh=0.;
+
+
+  float rcpar = deltaLnL - cthresh;
+
+
+  // also calculate angle between 2R rings (should be moved to separate function)
+  fqmrdot =   fqevent->fqmrdir[best2RID][0][0]*fqevent->fqmrdir[best2RID][1][0]
+            + fqevent->fqmrdir[best2RID][0][1]*fqevent->fqmrdir[best2RID][1][1]
+            + fqevent->fqmrdir[best2RID][0][2]*fqevent->fqmrdir[best2RID][1][2];
+
+
+  // signed sq root ///////////
+//  if (rcpar<0.){
+//    rcpar =  -1.*TMath::Sqrt(-1.*rcpar);
+//  }
+//  else{
+//    rcpar = TMath::Sqrt(rcpar);
+//  }
+  ////////////////////////////
+  // tricks to make peak widths in distribution more comparable
+//  return 500.*(TMath::Log(rcpar+40.)-TMath::Log(40));
+
+  return rcpar;
+}
+
+///////////////////////////////////////
+//returns the index of the best 2R fit
+// ! temporary change to return fit 20000033 !
+int getBest2RFitID(fqEvent* fqevent){
+
+  // total number of MR fits
+  int nfits = (int)fqevent->fqnmrfit;
+
+  // loop to find the best likelihood
+  double ngLnLBest = 10000000.;
+  int bestindex = 0;
+  for (int ifit=0;ifit<nfits;ifit++){
+    int fitID = TMath::Abs(fqevent->fqmrifit[ifit]); //< fit fit ID code
+    // pick out the fits we want to compare to
+    if ( TMath::Abs((TMath::Abs(fitID)-20000000))<50){
+      // check if it's the best
+      if (fqevent->fqmrnll[ifit] < ngLnLBest){
+        ngLnLBest = fqevent->fqmrnll[ifit];
+        bestindex = ifit;
+      }
+    }
+  }
+  best2RID = bestindex;
+  return bestindex;
+}
--- t2k_sk/detSystReweight.C
+++ t2k_sk/detSystReweight.C
@@ -1,7 +1,42 @@
 
 
+
+enum ATMPDEventType{
+  SubGeV_elike_0dcy = 1,
+  SubGeV_elike_1dcy,
+  SubGeV_SingleRing_pi0like,
+  SubGeV_mulike_0dcy,
+  SubGeV_mulike_1dcy,
+  SubGeV_mulike_2dcy,
+  SubGeV_pi0like,
+  MultiGeV_elike_nue,
+  MultiGeV_elike_nuebar,
+  MultiGeV_mulike,
+  MultiRing_elike_nue,
+  MultiRing_elike_nuebar,
+  MultiRing_mulike,
+  MultiRingOther_1,
+  PCStop, // not yet in data
+  PCThru,
+  UpStop_mu,
+  UpThruNonShower_mu,
+  UpThruShower_mu
+};
+
+struct fqEvent{
+  float*** fqmrdir;
+
+  float** fq1rnll;
+  float** fqmrmom;
+
+  float* fqmrnll;
+}
+
 std::vector<double> AtmSubGeVRecoEdges;
 std::vector<double> CosZenithRecoEdges;
+
+std::vector< std::vector<std::string> > cutObservableListForSample;
+std::vector< std::string > cutsList;
 
 void init();
 
@@ -17,6 +52,76 @@ void detSystReweight(){
 
 void init(){
 
+  // CUTS DEFINITION
+  // https://www.t2k.org/t2ksk/code/ntuplevariables
+  cutsList.emplace_back("evis"); // Reconstructed Visible Energy
+  cutsList.emplace_back("fq1rnll"); // LL for 1 ring
+  cutsList.emplace_back("fqmrnll"); // LL for multi rings
+  cutsList.emplace_back("fqpi0nll"); // LL for Pi0 id
+
+  // fqnse is the number of sub events, with the first being the lepton, and subsequent decay electrons
+  cutsList.emplace_back("fqnse - 1"); // Nb of decay electrons
+
+  cutsList.emplace_back("PID");
+  cutsList.emplace_back("Ndecay");
+  cutsList.emplace_back("PID of MER");
+  cutsList.emplace_back("MME LL");
+  cutsList.emplace_back("NueNuebarSeparationLL");
+  cutsList.emplace_back("Both rings are e-like and Minv in π0 range");
+
+  // SAMPLES DEFINITION
+  int nbSamples = UpThruShower_mu; // last one
+  cutObservableListForSample.resize(nbSamples);
+
+  // // SubGeV Samples
+  // cutObservableListForSample[ SubGeV_elike_0dcy ].emplace_back("Evis");
+  // cutObservableListForSample[ SubGeV_elike_0dcy ].emplace_back("Nrings");
+  // cutObservableListForSample[ SubGeV_elike_0dcy ].emplace_back("PID");
+  // cutObservableListForSample[ SubGeV_elike_0dcy ].emplace_back("Ndecay");
+  //
+  // cutObservableListForSample[ SubGeV_elike_1dcy ] = cutObservableListForSample[ SubGeV_elike_0dcy ];
+  //
+  // cutObservableListForSample[ SubGeV_mulike_0dcy ] = cutObservableListForSample[ SubGeV_elike_0dcy ];
+  // cutObservableListForSample[ SubGeV_mulike_1dcy ] = cutObservableListForSample[ SubGeV_elike_0dcy ];
+  // cutObservableListForSample[ SubGeV_mulike_2dcy ] = cutObservableListForSample[ SubGeV_elike_0dcy ];
+  //
+  // cutObservableListForSample[ MultiGeV_elike_nue ] = cutObservableListForSample[ SubGeV_elike_0dcy ];
+  // cutObservableListForSample[ MultiGeV_elike_nuebar ] = cutObservableListForSample[ SubGeV_elike_0dcy ];
+  //
+  // cutObservableListForSample[ SubGeV_pi0like ] = cutObservableListForSample[ SubGeV_elike_0dcy ];
+  // cutObservableListForSample[ SubGeV_pi0like ].pop_back();
+  // cutObservableListForSample[ SubGeV_pi0like ].emplace_back("Both rings are e-like and Minv in π0 range");
+  //
+  // // MultiGeV / SR samples
+  // cutObservableListForSample[ MultiGeV_mulike ].emplace_back("Evis");
+  // cutObservableListForSample[ MultiGeV_mulike ].emplace_back("Nrings");
+  // cutObservableListForSample[ MultiGeV_mulike ].emplace_back("PID");
+  //
+  // // MultiGeV / MR samples
+  // cutObservableListForSample[ MultiRing_mulike ].emplace_back("Evis");
+  // cutObservableListForSample[ MultiRing_mulike ].emplace_back("Nrings");
+  // cutObservableListForSample[ MultiRing_mulike ].emplace_back("PID of MER");
+  //
+  // cutObservableListForSample[ MultiRingOther ] = cutObservableListForSample[ MultiRing_mulike ];
+  // cutObservableListForSample[ MultiRingOther ].emplace_back("MME LL");
+  //
+  // cutObservableListForSample[ MultiRing_elike_nue ] = cutObservableListForSample[ MultiRingOther ];
+  // cutObservableListForSample[ MultiRing_elike_nue ].emplace_back("NueNuebarSeparationLL");
+  //
+  // cutObservableListForSample[ MultiRing_elike_nuebar ] = cutObservableListForSample[ MultiRing_elike_nue ];
+
+
+  // MultiGeV > 1.33 GeV
+  cutObservableListForSample[ MultiGeV_elike_nue ].emplace_back("evis");
+  cutObservableListForSample[ MultiGeV_elike_nue ].emplace_back("nrings"); // Single ring
+  // elike
+  cutObservableListForSample[ MultiGeV_elike_nue ].emplace_back("pid");
+  // 1 or more decay electron
+  // LLrat_nDcy_nueneb: number of decay electrons for nue/neubar separation
+  cutObservableListForSample[ MultiGeV_elike_nue ].emplace_back("LLrat_nDcy_nueneb");
+
+
+  // OSCILLATION FIT BINNING
   // binning edges from Fig. 7.2 in m.jiang's thesis
   const double tenTo2p0 =    100.0;
   const double tenTo2p4 =    251.2;
@@ -40,5 +145,6 @@ void init(){
     -1.00, -0.80, -0.60, -0.40, -0.20,
     0.00,  0.20,  0.40,  0.60,  0.80,  1.00
   };
+
 
 }
