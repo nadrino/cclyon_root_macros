ff8dfd0b41168fd766e61eeb3aa7869251a3b4bf
Adrien BLANCHET <adrien.blanchet@live.fr>
2020-11-03 10:04:25 +0100

    Fast commit.

--- t2k_sk/detSystParTest.C
+++ t2k_sk/detSystParTest.C
@@ -139,9 +139,10 @@ int getBest2RFitID(fqEvent* fqevent){
   int bestindex = 0;
   for (int ifit=0;ifit<nfits;ifit++){
     cout << GET_VAR_NAME_VALUE(ifit) << endl;
+
+    cout << GET_VAR_NAME_VALUE(fqevent->fqmrifit) << endl;
     int fitID = TMath::Abs(fqevent->fqmrifit[ifit]); //< fit fit ID code
 
-    cout << "HERE?" << endl;
     // pick out the fits we want to compare to
     if ( TMath::Abs((TMath::Abs(fitID)-20000000))<50){
       // check if it's the best
